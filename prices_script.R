3abcfc31e0a1d7a55f4944f558e124600f5406d1
J-Crookston <cfr45@msn.com>
2023-07-16 03:31:06 -0700

    Add files via upload

---
+++ prices_script.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(quantmod)
+library(lubridate)
+
+stock_df   <- readxl::read_excel("C:\\Users\\cfr45\\OneDrive\\Documents\\10-Programming\\01-R\\StockPrices\\prices.xlsx", sheet="Price")
+
+stock_df   <- stock_df %>% 
+  mutate(Last=(quantmod::getQuote(Stock)[[2]])) %>% 
+  mutate(Date=as.Date(quantmod::getQuote(Stock)[[1]], "%Y-%m-%d"))
+
+new <- quantmod::getQuote(stock_df$Stock) %>% 
+  rownames_to_column("Stock")
+
+write_csv(new, "C:\\Users\\cfr45\\OneDrive\\Documents\\10-Programming\\01-R\\StockPrices\\output.csv")
+
+cat("Script finished\n");
+
